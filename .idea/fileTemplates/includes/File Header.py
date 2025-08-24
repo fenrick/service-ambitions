764a3cacfe5175704eb6c0da6a183595668bab40
Jolyon Suthers <201621+fenrick@users.noreply.github.com>
2025-08-24 11:15:46 +1000

    chore: add MIT copyright and header templates

---
+++ .idea/fileTemplates/includes/File Header.py
@@ -0,0 +1,5 @@
+# Copyright (c) ${YEAR} Service Ambitions contributors
+#
+# This source code is licensed under the MIT License found in the
+# LICENSE file in the root directory of this source tree.
+#
